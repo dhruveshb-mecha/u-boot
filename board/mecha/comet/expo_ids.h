b418ad3395f3cfdb610f3ff38bf99929f76e8150
Advait Dhamorikar <advaitd@mechasystems.com>
2026-01-05 21:06:58 +0530

    boards: mecha: comet: add mecha comet developer menu expo layout

---
+++ board/mecha/comet/expo_ids.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Sample expo screen layout (ID numbers)
+ */
+
+enum {
+	ID_PROMPT = EXPOID_BASE_ID,
+
+	ID_SCENE1,
+	ID_SCENE1_TITLE,
+
+	ID_DEV_MENU,
+
+	ID_MENU_UBOOT,
+	ID_MENU_FASTBOOT,
+	ID_MENU_EXIT,
+
+	ID_DYNAMIC_START,
+};
